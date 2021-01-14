c4ed6c702c67db1a23e2e6da286d7c8961601bc7
Nikoloz Razmadze <raloz1@gmail.com>
2021-01-14 06:11:31 +0400

    Revert "Add chrome"
    This reverts commit 7c262fdfead3a55dd2db32021e6ed12ea37ecf14.

--- src/chart.ts
+++ src/chart.ts
@@ -1,18 +1,14 @@
 import path from 'path';
-import chrome from 'chrome-aws-lambda';
 import puppeteer from 'puppeteer';
 
+import { unsupportedCurrency } from './messages';
 import { sendPhoto } from './api';
 
 export async function chart(chatId: number, text: string): Promise<void> {
 
   const filePath = `file://${path.join(__dirname, '../res/index.html')}`;
 
-  const browser = await puppeteer.launch({
-    args: chrome.args,
-    executablePath: await chrome.executablePath,
-    headless: chrome.headless
-  });
+  const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
   return new Promise(async (resolve, reject) => {
