3fa0e3102e32d781fe0503c82c3f690f9733c97e
Nikoloz Razmadze <nikoloz@razmadze.me>
2023-12-27 03:08:35 +0400

    Don't export unimported function

--- src/db.js
+++ src/db.js
@@ -1,7 +1,7 @@
 import { open } from "sqlite";
 import sqlite3 from "sqlite3";
 
-export const getConnection = async () => {
+const getConnection = async () => {
   const db = await open({
     filename: "./data/db.sqlite",
     driver: sqlite3.cached.Database,
